8b9d4a2a415bd12b507553287c2413c8d9b50ae7
taicout6 <taicout6@gmail.com>
2022-04-08 06:30:38 -0300

    Basic structure

---
+++ extension.js
@@ -0,0 +1,13 @@
+// const St = imports.gt.St;
+// const Main = imports.ui.main;
+import St from 'gt.St';
+import Main from 'ui.main';
+
+let panelButton, panelButtonText;
+
+function init () {}
+
+function enable() {}
+
+function disable() {}
+
